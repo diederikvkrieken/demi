ec7accc4353d595d7dff581acd2e8ab1abcbfa85
diederik.van.krieken <diederikvkrieken@gmail.com>
2016-11-24 16:28:26 +0100

    Started cleaning

--- src/Demi/Controller.java
+++ src/Demi/Controller.java
@@ -35,12 +35,12 @@ public class Controller {
         while(!isConverge && i < nRounds) {
             System.out.println("Round #: "+i);
             mod.newRound(i);
-            run(method);
+            run(i, method);
             i++;
         }
     }
 
-    public void runSimulation(){
+    void runSimulation(){
         int i =1;
         //Each agent calculate their average weight.
         for (Agent a:mod.getAgents()) {
@@ -84,9 +84,7 @@ public class Controller {
             e.printStackTrace();
         }
     }
-
-
-    public void run(int t) {
+    void run(int t, int method) {
         //Start new proposal round
         /*Determine agent to propose*/
         int propose_agent = t % mod.getn_agents();
@@ -102,50 +100,53 @@ public class Controller {
             * We decide on a simplified world where no change occurs
             * */
 
-            //IF it is the agent which is supposed to create the offer
+            // Two options, if it is agent to do concession, make offer
+            // Else update the agent.
+
+            //If it is the agent which is supposed to create the offer
             if (agent.getName().equals(name)) {
                 System.out.println("Proposal by agent: " + name);
+
                 //Start reactive concession strategy
                 //Now calculate reactive concession for each agent
                 double deltaU[] = new double[mod.getn_agents()+1];
                 int i = 0;
                 double concession = Double.POSITIVE_INFINITY;
 
+                // Calculate concession
+                // This is dependent on the method called.
+                // 1 for normal reactive, 2 for nonreactive, 3 for only mixbed reactive
+                boolean nonreactive = false;
+                if (method == 2){
+                    nonreactive = true;
+                }
                 for (Agent ag : mod.getAgents()) {
-                    if ((agent.utility(ag.getOffer()) >= agent.getMinimumUtility())||(!ag.getName().equals("Mixbed"))) {
+                    if ((agent.utility(ag.getOffer()) >= agent.getMinimumUtility())||nonreactive) {
                         //Calculate nonreactive if offer >= minumum
                         deltaU[i] = agent.nonreactiveConcessionStrategyReturn(t);
                         i++;
-//                        System.out.println("nonreactive because offer is greater than minimum utiliy");
-//                        System.out.println("Utility of offer = " + agent.utility(ag.getOffer())+" and minimum util = "+agent.getMinimumUtility());
                     } else {
                         //calculate reactive if offer < minimum
-//                        System.out.println("reactive");
-//                        System.out.println("agent: "+mod.getAgentNumber(ag)+" Standing "+ mod.getStandingOffer(ag)+" x0: "+ mod.getRecentOffers(0).get(mod.getAgentNumber(ag))+" Last " + mod.getRecentOffers(t - 1).get(mod.getAgentNumber(agent)));
                         deltaU[i] = agent.reactiveConcessionStrategy(t, mod.getAgentNumber(ag), mod.getStandingOffer(ag), mod.getRecentOffers(0).get(mod.getAgentNumber(ag)), mod.getRecentOffers(t - 1).get(mod.getAgentNumber(agent)));
                         i++;
-//                        System.out.println(" reactive ");
                     }
                 }
+                // Get the normal concession strategy.
                 deltaU[i] = agent.nonreactiveConcessionStrategyReturn(t);
                 for (int j = 0; j < mod.getn_agents(); j++) {
-//                    System.out.println(deltaU[j]);
                     if (deltaU[j] < concession) {
                         concession = deltaU[j];
                     }
                 }
 
                 // Determining s_i
-//                System.out.println("Concession: " + concession);
                 agent.setDesirableUtility(concession);
 
                 //Calculating w
                 State weight = agent.calculateWeight(mod, t);
-//                System.out.println("The " + " weights is " + weight.toString());
 
                 State proposal;
                 //Projection P on indifference curve if needed
-//                System.out.println("agent(utility): "+agent.utility(weight)+" agent desirable = " +agent.getDesirableUtility());
                 if (agent.utility(weight) <= agent.getDesirableUtility()) {
                     proposal = agent.pointOnConcessionLine(weight);
                 } else{
@@ -198,9 +199,6 @@ public class Controller {
             isConverge = true;
         }
 
-//        Agent at =  mod.getAgents()[2];
-//        int wa = ((Mixbed)at).getWater_ratio();
-//        System.out.println("Mixbed "+wa);
 
         //For CSV file
         mod.addDistance(maxDistance);
@@ -208,4 +206,127 @@ public class Controller {
         mod.addDistanceAvg(t-1, distanceAvgArray);
         mod.addDesire(t-1, desireArray);
     }
+
+//    void run(int t) {
+//        //Start new proposal round
+//        /*Determine agent to propose*/
+//        int propose_agent = t % mod.getn_agents();
+//        String name = mod.getnameAgents(propose_agent);
+//        ArrayList<Double> concessionArray = new ArrayList<>();
+//        ArrayList<Double> distanceAvgArray = new ArrayList<>();
+//        ArrayList<Double> desireArray = new ArrayList<>();
+//
+//
+//        /*For each agent j in agents do:*/
+//        for (Agent agent : mod.getAgents()) {
+//            /*
+//            * We decide on a simplified world where no change occurs
+//            * */
+//
+//            //IF it is the agent which is supposed to create the offer
+//            if (agent.getName().equals(name)) {
+//                System.out.println("Proposal by agent: " + name);
+//                //Start reactive concession strategy
+//                //Now calculate reactive concession for each agent
+//                double deltaU[] = new double[mod.getn_agents()+1];
+//                int i = 0;
+//                double concession = Double.POSITIVE_INFINITY;
+//
+//                for (Agent ag : mod.getAgents()) {
+//                    if ((agent.utility(ag.getOffer()) >= agent.getMinimumUtility())||(!ag.getName().equals("Mixbed"))) {
+//                        //Calculate nonreactive if offer >= minumum
+//                        deltaU[i] = agent.nonreactiveConcessionStrategyReturn(t);
+//                        i++;
+////                        System.out.println("nonreactive because offer is greater than minimum utiliy");
+////                        System.out.println("Utility of offer = " + agent.utility(ag.getOffer())+" and minimum util = "+agent.getMinimumUtility());
+//                    } else {
+//                        //calculate reactive if offer < minimum
+////                        System.out.println("reactive");
+////                        System.out.println("agent: "+mod.getAgentNumber(ag)+" Standing "+ mod.getStandingOffer(ag)+" x0: "+ mod.getRecentOffers(0).get(mod.getAgentNumber(ag))+" Last " + mod.getRecentOffers(t - 1).get(mod.getAgentNumber(agent)));
+//                        deltaU[i] = agent.reactiveConcessionStrategy(t, mod.getAgentNumber(ag), mod.getStandingOffer(ag), mod.getRecentOffers(0).get(mod.getAgentNumber(ag)), mod.getRecentOffers(t - 1).get(mod.getAgentNumber(agent)));
+//                        i++;
+////                        System.out.println(" reactive ");
+//                    }
+//                }
+//                deltaU[i] = agent.nonreactiveConcessionStrategyReturn(t);
+//                for (int j = 0; j < mod.getn_agents(); j++) {
+////                    System.out.println(deltaU[j]);
+//                    if (deltaU[j] < concession) {
+//                        concession = deltaU[j];
+//                    }
+//                }
+//
+//                // Determining s_i
+////                System.out.println("Concession: " + concession);
+//                agent.setDesirableUtility(concession);
+//
+//                //Calculating w
+//                State weight = agent.calculateWeight(mod, t);
+////                System.out.println("The " + " weights is " + weight.toString());
+//
+//                State proposal;
+//                //Projection P on indifference curve if needed
+////                System.out.println("agent(utility): "+agent.utility(weight)+" agent desirable = " +agent.getDesirableUtility());
+//                if (agent.utility(weight) <= agent.getDesirableUtility()) {
+//                    proposal = agent.pointOnConcessionLine(weight);
+//                } else{
+//                    proposal = weight;
+//                }
+//
+//                //Ensure that proposal is range [0 1]
+//                proposal = agent.pointWithinRange(proposal);
+//
+//
+//                System.out.println("Proposal is "+proposal.toString());
+//
+//                //Propose
+//                mod.propose(agent, proposal, t);
+//                agent.addCurrentOffer(proposal);
+//
+//                desireArray.add(agent.getDesirableUtility());
+//                concessionArray.add(concession);
+//
+//            } else {
+//                System.out.println("\t Other agent update "+agent.getName());
+//
+//                //Add last offer tot the new list
+//                State x = agent.getOffer();
+//                State of = mod.getStandingOffer(propose_agent);
+//                agent.addCurrentOffer(x);
+//                mod.propose(agent, x, t);
+//
+//                //Update last best prev offer if necessary
+//                if (agent.utility(of) >= agent.utility(agent.getPrevBestOffer(propose_agent))){
+//                    agent.setPrevBestOffer(propose_agent, of);
+//                }
+//                desireArray.add(agent.getDesirableUtility());
+//            }
+//        }
+//        //Update the offer list for the agents
+//        for (Agent a :mod.getAgents()) {
+//            a.setPrevOffer(mod.getRecentOffers(t));
+//        }
+//        //Calculate distance
+//        double maxDistance = 0;
+//        for (Agent a:mod.getAgents()) {
+//            double distance = calculateDistance(a.getOffer(), a.getCurrentWeight());
+//            distanceAvgArray.add(distance);
+//            if (distance > maxDistance){
+//                maxDistance = distance;
+//            }
+//        }
+//        if (maxDistance < tolerance){
+//            isConverge = true;
+//        }
+//
+////        Agent at =  mod.getAgents()[2];
+////        int wa = ((Mixbed)at).getWater_ratio();
+////        System.out.println("Mixbed "+wa);
+//
+//        //For CSV file
+//        mod.addDistance(maxDistance);
+//        mod.addConcession(t-1, concessionArray);
+//        mod.addDistanceAvg(t-1, distanceAvgArray);
+//        mod.addDesire(t-1, desireArray);
+//    }
 }
